1d795c447e49805d7c3d7f46db6c95c0bc934db3
DamonV <vdv_box@mail.ru>
2017-03-21 19:43:45 +0300

    1

--- bin/www.js
+++ bin/www.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var app = require('../app');
 var debug = require('debug')('testing:server');
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
 
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3000');
+var port = 3000;//normalizePort(process.env.PORT || '3000');
 app.set('port', port);
+var port2 = 7000;//443
 
-/**
- * Create HTTP server.
- */
+var options = {
+    key: fs.readFileSync('nodekey.pem'),
+    cert: fs.readFileSync('nodecert.pem')
+};
 
+// Create HTTP server.
 var server = http.createServer(app);
+
+// Create HTTPS server
+var server2 = https.createServer(options, app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -29,11 +32,14 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+server2.listen(port2);
+server2.on('error', onError2);
+server2.on('listening', onListening2);
+
 /**
  * Normalize a port into a number, string, or false.
  */
-
-function normalizePort(val) {
+/*function normalizePort(val) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -47,21 +53,18 @@ function normalizePort(val) {
   }
 
   return false;
-}
+}*/
 
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
-
   var bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
@@ -76,15 +79,43 @@ function onError(error) {
       throw error;
   }
 }
-
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
+}
+
+// HTTPS
+function onError2(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = typeof port2 === 'string'
+        ? 'Pipe ' + port2
+        : 'Port ' + port2;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+function onListening2() {
+    var addr = server2.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
